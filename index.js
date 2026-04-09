5c2acfded5cc243b06692e07a8aa1958275b19c5
Aashesh Kumar <Aashesh.sharma389@gmail.com>
2026-04-10 01:49:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,35 +6,36 @@ toggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 const form = document.querySelector("form");
+const message = document.getElementById("form-message");
 
-form.addEventListener("submit", async (e) => {
+form.addEventListener("submit", async function (e) {
   e.preventDefault();
 
-  const data = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    message: document.getElementById("message").value
-  };
-
   try {
-    const res = await fetch("http://localhost:5000/send", {
+    const res = await fetch("YOUR_API_URL", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
+      body: new FormData(form)
     });
 
     const result = await res.json();
 
     if (result.success) {
-      alert("Message sent successfully!");
-      form.reset(); // clear form
+      message.style.display = "block";
+      message.style.color = "lightgreen";
+      message.textContent = "I'll get back to you soon 🙂";
+
+      form.reset();
     } else {
-      alert("Failed to send message");
+      message.style.display = "block";
+      message.style.color = "red";
+      message.textContent = "Failed to send message";
     }
+
   } catch (error) {
     console.error(error);
-    alert("Server error");
+
+    message.style.display = "block";
+    message.style.color = "red";
+    message.textContent = "Server error";
   }
 });
